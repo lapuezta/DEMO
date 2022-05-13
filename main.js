53129cc96974606a5cb98419db286a72ea4fe8b0
lapuezta <104935742+lapuezta@users.noreply.github.com>
2022-05-13 14:04:37 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+// OLDSCHOOL
+var ichBinEineVar = "Hallo Var";
+
+// NEWSCHOOL
+const ichBinEineConst = "Hello Const";
+let ichBinEineLet = "Hello Let";
+
+// Die value von const kann nicht verändert werden
+console.log(ichBinEineConst);
+// ichBinEineConst = "hellooooooo";
+console.log(ichBinEineConst);
+
+// Die value von let kann verändert werden
+console.log(ichBinEineLet);
+ichBinEineLet = "Hallloooooo";
+console.log(ichBinEineLet);
+
